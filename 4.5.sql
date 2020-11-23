3733d083061a52b79115803782e5ad4ad78e9dcd
aya-kudoh <a1021bn@outlook.jp>
2020-11-23 12:37:28 +0000

    4章内容追加

--- 4.5.sql
+++ 4.5.sql
@@ -1,1 +1,17 @@
+/*1つのカラムのデータを取得*/
+select email from training1.users;
+
+/*複数カラムのデータの取得*/
+select name, age, id from training1.users;
+
+/*すべてのカラムのデータを取得*/
+select * from training1.users
+
+/*
+--------------------------------------------------------------------------------
+演習4.5
+training1 DB の usersテーブルから、左から順に id, name, tel, email の
+カラムを並べてデータを取得するSELECT文を記述してください。
+---------------------------------------------------------------------------------
+*/
 SELECT name, email, tel from training1.users where email='jiro.tanaka@ccc.com';
