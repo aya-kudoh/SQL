5e99ef4670464c0a4d4054413313eb2b5781bdd7
aya-kudoh <a1021bn@outlook.jp>
2020-11-24 04:40:52 +0000

    6章・7章内容追加

--- 6.5.sql
+++ 6.5.sql
@@ -1,1 +1,20 @@
+/*
+ORDER BY の使い方
+SELECT 文の中で ORDER BYを使用することで、指定したカラムの値を並び替えたデータを取得することができます。
+asc : 昇順　/  desc : 降順
+*/
+select id, name, height from training1.users order by height asc;
+
+/*
+ORDER BY は WHERE と組み合わせることで、特定のデータを並び替えた状態で取得できるようになります。
+*/
+select * from training1.users where age >= 20 order by gender asc, age desc;
+
+/*
+-------------------------------------------------------------------------------------------------
+演習6.5
+training1 DB の users テーブルから、height カラム値が160以上であるユーザーの全てのデータを、
+weight カラム値の昇順に並び替えて取得してください。
+-------------------------------------------------------------------------------------------------
+*/
 SELECT * from training1.users where height  >= 160 order by weight asc;
